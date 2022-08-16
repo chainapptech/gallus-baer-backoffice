e199510521a4995261a8a4f6115d421078c77e77
sstefdev <sstefandev@gmail.com>
2022-08-16 11:43:33 +0200

    removed TS and TS files

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import { Auth } from "aws-amplify";
 import GlobalWrapper from "components/GlobalWrapper";
 
 function App() {
- // const [isAuthenticating, setIsAuthenticating] = useState(true);
-  const [isAuthenticated, userHasAuthenticated] = useState(false);
-  useEffect(() => {
-    onLoad();
-  }, []);
-  
-  async function onLoad() {
-    try {
-      await Auth.currentSession();
-      userHasAuthenticated(true);
-    } catch (e) {
-      if (e !== "No current user") {
-        alert(e);
-      }
-    }
-  
-    //setIsAuthenticating(false);
-  }
+  // const [isAuthenticating, setIsAuthenticating] = useState(true);
+  // const [isAuthenticated, userHasAuthenticated] = useState(false);
+  // useEffect(() => {
+  //   onLoad();
+  // }, []);
+
+  // async function onLoad() {
+  //   try {
+  //     await Auth.currentSession();
+  //     userHasAuthenticated(true);
+  //   } catch (e) {
+  //     if (e !== "No current user") {
+  //       alert(e);
+  //     }
+  //   }
+
+  //   //setIsAuthenticating(false);
+  // }
   return (
-    <AppContext.Provider
-    value={{ isAuthenticated, userHasAuthenticated }}
-  >
-    <GlobalWrapper />
-  </AppContext.Provider>
+    <AppContext.Provider value={{}}>
+      <GlobalWrapper />
+    </AppContext.Provider>
   );
 }
 
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Row, Col, Form } from 'react-bootstrap';
+import { Row, Col, Form } from "react-bootstrap";
 import LoaderButton from "../../components/LoaderButton";
 import { useAppContext } from "../../lib/contextLib";
-import { onError } from '../../lib/errorLib';
-import { Auth } from 'aws-amplify';
-import './Login.scss';
-import Button from 'components/Button';
+import { onError } from "../../lib/errorLib";
+import { Auth } from "aws-amplify";
+import "./Login.scss";
+import Button from "components/Button";
 // interface UserCredentials {
 //   email: string;
 //   password: string;
 // }
 
 const Login = () => {
-  const { userHasAuthenticated } = useAppContext();
+  // const { userHasAuthenticated } = useAppContext();
   const nav = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -23,56 +23,50 @@ const Login = () => {
     return email.length > 0 && password.length > 0;
   }
 
-    async function handleSubmit(event) {
-      event.preventDefault();
-      setIsLoading(true);
-    
-      try {
-        await Auth.signIn(email, password);
-        userHasAuthenticated(true);
-        nav("/dashboard");
-      } catch (e) {
-        // let errorMessage = "Failed to do something exceptional";
-        if (e instanceof Error) {
-          // errorMessage = e.message;
-          onError(e);
-          setIsLoading(false);
-        } else {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await Auth.signIn(email, password);
+      // userHasAuthenticated(true);
+      nav("/dashboard");
+    } catch (e) {
+      // let errorMessage = "Failed to do something exceptional";
+      if (e instanceof Error) {
+        // errorMessage = e.message;
+        onError(e);
+        setIsLoading(false);
+      } else {
         alert("Unexpected error");
-        }
       }
     }
+  }
 
   return (
     <Row className="no-gutters center login-page">
-      <Col md={7} className="login-img">
-        
-      </Col>
-      <Col
-        md={5}
-        className="d-flex flex-column justify-items-center"
-      >
+      <Col md={7} className="login-img"></Col>
+      <Col md={5} className="d-flex flex-column justify-items-center">
         <img src="/assets/logo.png" alt="logo" className="logo mb-5" />
         <h1 className="mb-4">Login</h1>
-        
+
         <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="email" className='my-3'>
+          <Form.Group controlId="email" className="my-3">
             <Form.Label>Username or Email</Form.Label>
             <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+              autoFocus
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Group>
-          <Form.Group controlId="password" className='mb-3'>
+          <Form.Group controlId="password" className="mb-3">
             <Form.Label>Password</Form.Label>
             <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-           
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Form.Group>
           <Button>Login</Button>
           <br />
