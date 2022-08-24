42ba141fba8405b5fc77cf6dbc162e4da38cf1be
sstefdev <sstefandev@gmail.com>
2022-08-24 13:21:53 +0200

    sidebar links fix

--- src/components/SidebarLink/index.js
+++ src/components/SidebarLink/index.js
@@ -1,28 +1,21 @@
-import React, { useState } from "react";
-import NotificationBadge from "../NotificationBadge";
+import { useLocation } from "react-router-dom";
 
+import NotificationBadge from "../NotificationBadge";
 import "./styles.scss";
 
 const SidebarLink = ({ link }) => {
-  const [showFillIcon, setShowFillIcon] = useState(false);
-
-  function showFillIconHandler() {
-    setShowFillIcon(true);
-  }
-
-  function hideFillIconHandler() {
-    setShowFillIcon(false);
-  }
+  const { pathname } = useLocation();
+  const isLinkActive = pathname === link.path;
 
   return (
     <button
-      onMouseDown={showFillIconHandler}
-      onMouseUp={hideFillIconHandler}
-      className="d-flex mb-2 px-3 sidebar-links"
+      className={`d-flex mb-4 py-2 px-3 sidebar-links ${
+        isLinkActive && "active-link"
+      }`}
     >
       <div className="d-flex justify-content-between align-items-center inner-button">
         <div className="d-flex align-items-center">
-          {showFillIcon ? link.active_icon : link.icon}
+          {isLinkActive ? link.active_icon : link.icon}
           <p className="mb-0 link-text">{link.text}</p>
         </div>
         {link.notificationNumber > 0 ? (
