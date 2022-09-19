b464bf770926ba582b2d407cfb0f24e938044f24
nikolapetrovicc <nikola94kv@gmail.com>
2022-09-19 11:18:07 +0200

    GAL-153-fixed-GAL-161-And-GAL-162-InquiryPageFinishedAndOptimizedForMobile

--- src/containers/Inqueries/Inquiries.js
+++ src/containers/Inqueries/Inquiries.js
@@ -7,8 +7,41 @@ import inquiries from "./inqueries-data.json";
 import Button from "components/Button";
 import ArrowRight from "stories/svg/ArrowRight";
 import ArrowDown from "stories/svg/ArrowDown";
+import InquiryCard from "components/InquiryCard";
+import useWindowSize from "lib/useWindowSize";
+
+const table = (item) => {
+  <Table striped>
+    <thead>
+      <tr>
+        <th>
+          <h6>User name & email adress</h6>
+        </th>
+        <th>
+          <h6>Property name</h6>
+        </th>
+        <th>
+          <h6>Date name</h6>
+        </th>
+        <th>
+          <h6>Time</h6>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      {item?.inquiry?.map((inquiryItem) => (
+        <InquiriesTableRow
+          key={inquiryItem.account.email}
+          inquiryItem={inquiryItem}
+        />
+      ))}
+    </tbody>
+  </Table>;
+};
 
 const Inquiries = () => {
+  const size = useWindowSize();
+
   return (
     <Row className="inquires-wrapper">
       <Col sm={12} md={12}>
@@ -31,7 +64,7 @@ const Inquiries = () => {
                 <Accordion.Item key={item?.id} eventKey={item?.id}>
                   <Accordion.Header>
                     <img
-                      className="logo"
+                      className="logo d-none d-md-block"
                       src={`assets/${item?.logo}`}
                       alt="Comparis"
                       width={"80px"}
@@ -42,39 +75,52 @@ const Inquiries = () => {
                     <PropertyTag type={"inquiries"} className={"me-2"}>
                       7 new
                     </PropertyTag>
-                    <PropertyTag type={"inquiries"}>31 total</PropertyTag>
+                    <PropertyTag
+                      type={"inquiries"}
+                      className={"d-none d-md-block"}
+                    >
+                      31 total
+                    </PropertyTag>
                   </Accordion.Header>
                   <Accordion.Body>
-                    <Table striped>
-                      <thead>
-                        <tr>
-                          <th>
-                            <h6>User name & email adress</h6>
-                          </th>
-                          <th>
-                            <h6>Property name</h6>
-                          </th>
-                          <th>
-                            <h6>Date name</h6>
-                          </th>
-                          <th>
-                            <h6>Time</h6>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {item?.inquiry?.map((inquiryItem) => (
-                          <InquiriesTableRow
-                            key={inquiryItem.account.email}
-                            inquiryItem={inquiryItem}
-                          />
-                        ))}
-                      </tbody>
-                    </Table>
+                    {size.width < 576 ? (
+                      <>
+                        <InquiryCard />
+                        <InquiryCard />
+                        <InquiryCard />
+                      </>
+                    ) : (
+                      <Table striped>
+                        <thead>
+                          <tr>
+                            <th>
+                              <h6>User name & email adress</h6>
+                            </th>
+                            <th>
+                              <h6>Property name</h6>
+                            </th>
+                            <th>
+                              <h6>Date name</h6>
+                            </th>
+                            <th>
+                              <h6>Time</h6>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {item?.inquiry?.map((inquiryItem) => (
+                            <InquiriesTableRow
+                              key={inquiryItem.account.email}
+                              inquiryItem={inquiryItem}
+                            />
+                          ))}
+                        </tbody>
+                      </Table>
+                    )}
                     <Row>
                       <Col className="d-flex align-items-center justify-content-end">
                         <div>
-                          <Link to={"/singleportal"}>
+                          <Link to={"/single-inquiry"}>
                             <Button
                               type="text"
                               endIcon={<ArrowRight fill="#063561" />}
--- src/containers/Inqueries/SingleInquiryPage/index.js
+++ src/containers/Inqueries/SingleInquiryPage/index.js
@@ -17,6 +17,7 @@ import ProfileIcon from "components/ProfileIcon";
 import Phone from "stories/svg/Phone";
 import Time from "stories/svg/Time";
 import InquiryCard from "components/InquiryCard";
+import InputComponent from "components/Input";
 
 const selectOptions = ["Not answered", "Answered", "Assigned"];
 
@@ -27,7 +28,7 @@ const SingleInquiryPage = () => {
     <Row className="mt-4 single-inquiry-page-wrapper">
       <Col sm={12} md={9}>
         <Row>
-          <Col sm={12} md={9}>
+          <Col sm={12} md={10}>
             <Breadcrumb className="mt-4 ">
               <Breadcrumb.Item>
                 <Link to={"/inquiries"}>Inquiries</Link>
@@ -35,136 +36,48 @@ const SingleInquiryPage = () => {
               <Breadcrumb.Item active>Immoscout 24</Breadcrumb.Item>
             </Breadcrumb>
             <h3 className="mt-2">Immoscout 24</h3>
+            <Row>
+              <Col sm={12} className="d-flex d-md-none">
+                <img
+                  src="../assets/LogoImmoScout24.png"
+                  alt="Logo"
+                  className="inquiries-logo-company-phone"
+                ></img>
+              </Col>
+            </Row>
 
             <Tabs
               defaultActiveKey="new-inquiries"
               id="uncontrolled-tab-example"
-              className="mb-4 mt-4 "
+              className="mt-0 mb-4 mt-md-4"
             >
               <Tab
                 eventKey="new-inquiries"
                 title="New inquiries"
                 tabClassName="position-relative pt-3 pb-3 ps-4 pe-4 bg-transparent"
               >
-                {/* Inquiries */}
-                <div className="single-inquiry-card">
-                  <Row>
-                    <Col md={9}>
-                      <Row>
-                        <Col
-                          md={12}
-                          className="d-flex align-items-center justify-content-start info-inquiry"
-                        >
-                          <ProfileIcon
-                            className={"profile-pict"}
-                            name="Jenna"
-                            surname="Jonnes"
-                            image={"../assets/account-1.jpg"}
-                          />
-                          <div>
-                            <h4>
-                              {/* {inquiryItem.account.name}{" "}
-                            {inquiryItem.account.surname} */}
-                              Jenna Jonnes
-                            </h4>
-                            <p className="link">
-                              {/* {inquiryItem.account.email &&
-                              inquiryItem.account.email[0]} */}
-                              jennajones@gmail.com
-                            </p>
-                          </div>
-                        </Col>
-                        <Col
-                          md={12}
-                          className="d-flex align-items-center justify-content-start phone-date-time"
-                        >
-                          <div className="d-flex align-items-center justify-content-start me-3">
-                            <div className="me-2">
-                              <Phone />
-                            </div>
-                            <p>+41 467 6655</p>
-                          </div>
-                          <div className="d-flex align-items-center justify-content-start ms-1 me-3">
-                            <div className="me-2">
-                              <CalendarDotted />
-                            </div>
-                            <p>13.03.2022.</p>
-                          </div>
-                          <div className="d-flex align-items-center justify-content-start ms-1">
-                            <div className="me-2">
-                              <Time />
-                            </div>
-                            <p>20:10</p>
-                          </div>
-                        </Col>
-                        <Col
-                          md={12}
-                          className="d-flex align-items-center justify-content-start"
-                        >
-                          <p className="me-1">Property:</p>
+                <Row className="inquiries-search">
+                  <Col sm={12} md={10}>
+                    <InputComponent placeholder="Search..." type={"search"} />
+                  </Col>
+                </Row>
 
-                          <p className="inquire-address">
-                            <b>14 NE 13th St, Miami</b>
-                          </p>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col md={3}>
-                      <Form.Group>
-                        <Form.Label className="m-0">
-                          <h5 className="ms-2">Inquiry status</h5>
-                        </Form.Label>
-                        <Form.Select
-                          className="mb-3"
-                          value={option}
-                          onChange={(e) => setOption(e.target.value)}
-                        >
-                          {selectOptions.map((item, index) => (
-                            <option key={index} value={item}>
-                              {item}
-                            </option>
-                          ))}
-                        </Form.Select>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md={12} className="single-inquiry-paragraph">
-                      <p>
-                        Etiam dictum nisi sed dui pretium facilisis. Sed nec
-                        nulla ullamcorper, iaculis mi et, tincidunt urna.
-                        Aliquam eget velit eget elit facilisis rhoncus quis at
-                        ligula. Curabitur vitae feugiat neque. Suspendisse
-                        dapibus rutrum metus, id malesuada nibh maximus
-                        accumsan. Proin condimentum metus eget venenatis mollis.
-                      </p>
-                    </Col>
-                    <Col md={9}>
-                      <Row>
-                        <Col>
-                          <Button
-                            type="secondary"
-                            leadingIcon={<CalendarDotted />}
-                          >
-                            Make an appointment
-                          </Button>
-                        </Col>
-                        <Col>
-                          <Button leadingIcon={<SendOutlined fill="#EBD3BD" />}>
-                            Send mail
-                          </Button>
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
-                </div>
-                {/* Inquiries */}
+                <InquiryCard />
+                <InquiryCard />
+                <InquiryCard />
               </Tab>
               <Tab
                 eventKey="answered-inquiries"
                 title="Answered inquiries"
                 tabClassName="position-relative pt-3 pb-3 ps-4 pe-4 bg-transparent"
               >
+                <Row className="inquiries-search">
+                  <Col sm={12} md={10}>
+                    <InputComponent placeholder="Search..." type={"search"} />
+                  </Col>
+                </Row>
+                <InquiryCard />
+                <InquiryCard />
                 <InquiryCard />
               </Tab>
             </Tabs>
@@ -174,7 +87,7 @@ const SingleInquiryPage = () => {
       <Col
         sm={12}
         md={3}
-        className="d-flex align-items-start justify-content-end"
+        className="align-items-start justify-content-end d-none d-md-flex"
       >
         <img
           src="../assets/LogoImmoScout24.png"
