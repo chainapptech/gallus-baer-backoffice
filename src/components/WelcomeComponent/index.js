4d91bef2b6a42c011cf2d44ab963ee5b6b7deaf5
Nikola Tesovic <ntesovic27@gmail.com>
2022-09-27 14:50:33 +0200

    Dashboard response fix and updated Document table

--- src/components/WelcomeComponent/index.js
+++ src/components/WelcomeComponent/index.js
@@ -12,38 +12,36 @@ const WelcomeComponent = () => {
             <h2>Lucia Morisson</h2>
           </div>
           <Row className="welcome-statistic mb-4">
-            <Col md={4}>
+            <Col md={4} className="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0">
               <h3>24</h3>
               <p>new properties</p>
             </Col>
-            <Col md={4}>
+            <Col md={4} className="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0">
               <h3>32</h3>
               <p>new inquiries</p>
             </Col>
-            <Col md={4}>
+            <Col md={4} className="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0">
               <h3>8</h3>
               <p>new arranged viewings</p>
             </Col>
           </Row>
-          <Row className="welcome-statistic">
-            <Col md={4}>
+          <Row>
+            <Col md={4} className="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0">
               <h3>14</h3>
               <p>new accounts</p>
             </Col>
-            <Col md={4}>
+            <Col md={4} className="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0">
               <h3>4</h3>
               <p>new portal ads</p>
             </Col>
-            <Col md={4}>
+            <Col md={4} className="d-flex flex-column align-items-center align-items-md-start mb-3 mb-md-0">
               <h3>4</h3>
               <p>new successful viewings</p>
             </Col>
           </Row>
         </Col>
-        <Col md={4} className="d-flex align-items-center justify-content-end">
-          <div className="welcome-ilustration">
+        <Col md={4} className="pt-4 pt-md-0 mt-4 mt-md-0 d-flex align-items-center justify-content-center justify-content-md-end">
             <WelcomeIlustration />
-          </div>
         </Col>
       </Row>
     </Container>
